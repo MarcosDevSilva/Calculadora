f93771e0948f1311a6718277add10ac2f6950ac2
Marco <tassio50000@gmail.com>
2024-11-27 21:13:26 -0300

    add functions calc

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+const somar = (a, b) => {
+    return a + b
+}
+
+const sub = (a, b) => {
+    return a - b
+}
+
+const multiply = (a, b) => {
+    return a * b
+}
+
+const divide = (a, b) => {
+    return a / b
+}
+
+console.log(somar(1,5 ))
+console.log(sub(10, 5))
+console.log(multiply(10 , 10))
+console.log(divide(8, 2))
